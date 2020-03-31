870f8848d1e4dc0e4a4e6196d82878d48259bd0b
Jordi <jordi.sabanes@students.salle.url.edu>
2020-04-01 00:37:48 +0200

    Test PlaySong Layout

--- app/src/main/java/vidal/sergi/sallefyv1/controller/activity/MainActivity.java
+++ app/src/main/java/vidal/sergi/sallefyv1/controller/activity/MainActivity.java
@@ -2,8 +2,6 @@ package vidal.sergi.sallefyv1.controller.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
