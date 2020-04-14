3f107d79e9dd80000799fc7f1fdff99a3150db03
Jordi <jordi.sabanes@students.salle.url.edu>
2020-04-14 23:21:07 +0200

    Cloudinary API implementation

--- app/src/main/java/vidal/sergi/sallefyv1/controller/activity/TrackLibraryActivity.java
+++ app/src/main/java/vidal/sergi/sallefyv1/controller/activity/TrackLibraryActivity.java
@@ -390,7 +390,7 @@ public class TrackLibraryActivity extends AppCompatActivity implements TrackList
 
     @Override
     public void onCreateTrack() {
-        
+
     }
 
     @Override
--- app/src/main/java/vidal/sergi/sallefyv1/utils/CloudinaryConfigs.java
+++ app/src/main/java/vidal/sergi/sallefyv1/utils/CloudinaryConfigs.java
@@ -6,9 +6,9 @@ public class CloudinaryConfigs {
 
     public static HashMap getConfigurations() {
         HashMap  config = new HashMap();
-        config.put("cloud_name", "username" );
-        config.put("api_key", "your_api_key");
-        config.put("api_secret", "your_api_secret");
+        config.put("cloud_name", "jordiism" );
+        config.put("api_key", "249513475856772");
+        config.put("api_secret", "mL5uUPHwI_hd7ixhHQUk9qiQyjQ");
         return config;
     }
 }
