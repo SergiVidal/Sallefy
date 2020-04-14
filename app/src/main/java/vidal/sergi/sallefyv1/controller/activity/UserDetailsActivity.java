b5fe9b2d9c0855587c36e52d7dff9f344744d3c2
Sergi Vidal Mestre <s.vidal@students.salle.url.edu>
2020-04-14 18:24:16 +0200

    #21 Modified User Details Activity

--- app/src/main/java/vidal/sergi/sallefyv1/controller/activity/UserDetailsActivity.java
+++ app/src/main/java/vidal/sergi/sallefyv1/controller/activity/UserDetailsActivity.java
@@ -3,6 +3,10 @@ package vidal.sergi.sallefyv1.controller.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 import java.util.ArrayList;
 
@@ -13,6 +17,13 @@ import vidal.sergi.sallefyv1.model.User;
 public class UserDetailsActivity extends AppCompatActivity {
 
     User user;
+    private ImageView ivUserPhoto;
+    private TextView tvUsername;
+    private TextView tvEmail;
+    private TextView tvNumPlaylist;
+    private TextView tvNumTracks;
+    private TextView tvNumFollowers;
+    private TextView tvNumFollowing;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +35,29 @@ public class UserDetailsActivity extends AppCompatActivity {
     }
 
     private void initViews() {
+        ivUserPhoto = findViewById(R.id.ivPlaylistPhoto);
+        if (user.getImageUrl() != null && !user.getImageUrl().equals("")) {
+            Glide.with(getApplicationContext())
+                    .asBitmap()
+                    .placeholder(R.drawable.ic_audiotrack)
+                    .load(user.getImageUrl())
+                    .into(ivUserPhoto);
+        }
+        tvUsername = findViewById(R.id.tvUsername);
+        tvUsername.setText(user.getFirstName());
 
+        tvEmail = findViewById(R.id.tvEmail);
+        tvEmail.setText(user.getEmail());
+
+//        tvNumPlaylist = findViewById(R.id.tvNumPlaylist);
+//        tvNumPlaylist.setText(String.valueOf(user.getPlaylists()));
+//
+//        tvNumTracks = findViewById(R.id.tvNumTracks);
+//        tvNumTracks.setText(String.valueOf(user.getTracks()));
+//
+//        tvNumFollowers = findViewById(R.id.tvNumFollowers);
+//        tvNumFollowers.setText(String.valueOf(user.getFollowers()));
+//        tvNumFollowing = findViewById(R.id.tvNumFollowing);
+//        tvNumFollowing.setText(String.valueOf(user.getFollowing()));
     }
 }
