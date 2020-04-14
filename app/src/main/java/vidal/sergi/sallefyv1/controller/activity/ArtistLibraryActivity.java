acaac6140fee67e17155d5df9c625bc3913afc13
joancc97 <joan-cat@hotmail.com>
2020-04-14 18:26:51 +0200

    Library Layout + PlayList + Song + addSong

---
+++ app/src/main/java/vidal/sergi/sallefyv1/controller/activity/ArtistLibraryActivity.java
@@ -0,0 +1,153 @@
+package vidal.sergi.sallefyv1.controller.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import vidal.sergi.sallefyv1.R;
+import vidal.sergi.sallefyv1.controller.adapters.UserAdapter;
+import vidal.sergi.sallefyv1.model.User;
+import vidal.sergi.sallefyv1.model.UserToken;
+import vidal.sergi.sallefyv1.restapi.callback.UserCallback;
+import vidal.sergi.sallefyv1.restapi.manager.UserManager;
+
+public class ArtistLibraryActivity extends AppCompatActivity implements UserCallback{
+    private BottomNavigationView mNav;
+    private RecyclerView mUsersView;
+    private UserAdapter mUserAdapter;
+    private Button bPlaylist;
+    private Button bCanciones;
+    private Button baddSong;
+
+    private int pos;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_artist_library);
+
+        getData();
+        initViews();
+
+        bPlaylist= (Button)findViewById(R.id.item_playlist_button);
+        bCanciones=  (Button)findViewById(R.id.item_canciones_button);
+        baddSong = (Button)findViewById(R.id.add_song);
+        bPlaylist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ArtistLibraryActivity.class);
+                startActivity(intent);
+            }
+        });
+        bCanciones.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), TrackLibraryActivity.class);
+                startActivity(intent);
+            }
+        });
+        baddSong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), UploadActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+
+    }
+    public void onDestroy() {
+        super.onDestroy();
+    }
+    private void getData() {
+        UserManager.getInstance(getApplicationContext())
+                .getUsers(this);
+    }
+
+    private void initViews() {
+
+        LinearLayoutManager managerPlaylists = new LinearLayoutManager(getApplicationContext(), RecyclerView.HORIZONTAL, false);
+        mUserAdapter = new UserAdapter(null, getApplicationContext());
+        mUsersView = (RecyclerView) findViewById(R.id.search_users_recyclerview);
+        mUsersView.setLayoutManager(managerPlaylists);
+        mUsersView.setAdapter(mUserAdapter);
+
+
+        mNav = findViewById(R.id.bottom_navigation);
+        mNav.setSelectedItemId(R.id.action_home);
+        mNav.setOnNavigationItemSelectedListener(menuItem -> {
+            Intent intent;
+            switch (menuItem.getItemId()) {
+                case R.id.action_home:
+                    intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                    break;
+                case R.id.action_search:
+                    intent = new Intent(getApplicationContext(), SearchActivity.class);
+                    startActivity(intent);
+                    break;
+                case R.id.action_profile:
+                    intent = new Intent(getApplicationContext(), ProfileActivity.class);
+                    startActivity(intent);
+                    break;
+
+            }
+            return true;
+        });
+
+    }
+    @Override
+    public void onLoginSuccess(UserToken userToken) {
+
+    }
+
+    @Override
+    public void onLoginFailure(Throwable throwable) {
+
+    }
+
+    @Override
+    public void onRegisterSuccess() {
+
+    }
+
+    @Override
+    public void onRegisterFailure(Throwable throwable) {
+
+    }
+
+    @Override
+    public void onUserInfoReceived(User userData) {
+
+    }
+
+    @Override
+    public void onUsersReceived(List<User> users) {
+        mUserAdapter = new UserAdapter((ArrayList<User>) users, getApplicationContext());
+        mUsersView.setAdapter(mUserAdapter);
+    }
+
+    @Override
+    public void onUsersFailure(Throwable throwable) {
+
+    }
+
+    @Override
+    public void onFailure(Throwable throwable) {
+
+    }
+}
