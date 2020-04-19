a6dd6755e582df5fabd69de342b6bbdc850b845b
joancc97 <joan-cat@hotmail.com>
2020-04-19 12:24:42 +0200

    Library Layout + PlayList + Song + addSong

--- app/src/main/java/vidal/sergi/sallefyv1/controller/activity/TrackOptionsActivity.java
+++ app/src/main/java/vidal/sergi/sallefyv1/controller/activity/TrackOptionsActivity.java
@@ -12,5 +12,6 @@ public class TrackOptionsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_track_options);
+
     }
 }
