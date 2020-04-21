77785adbfe7415607c7a1c6c0ce06d63d8f3e7fb
Sergi Vidal Mestre <s.vidal@students.salle.url.edu>
2020-04-21 18:20:51 +0200

    Created fragments

--- app/src/main/java/vidal/sergi/sallefyv1/controller/activity/MainActivity.java
+++ app/src/main/java/vidal/sergi/sallefyv1/controller/activity/MainActivity.java
@@ -1,14 +1,148 @@
 package vidal.sergi.sallefyv1.controller.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import vidal.sergi.sallefyv1.R;
+import vidal.sergi.sallefyv1.controller.callbacks.FragmentCallback;
+import vidal.sergi.sallefyv1.utils.Constants;
+import vidal.sergi.sallefyv1.utils.Session;
+
+public class MainActivity extends FragmentActivity implements FragmentCallback {
+
+    private FragmentManager mFragmentManager;
+    private FragmentTransaction mTransaction;
+
+    private BottomNavigationView mNav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        initViews();
+//        setInitialFragment();
+//        requestPermissions();
     }
+
+//    private void initViews() {
+//        mFragmentManager = getSupportFragmentManager();
+//        mTransaction = mFragmentManager.beginTransaction();
+//
+//        mNav = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+//        mNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+//            @Override
+//            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+//                Fragment fragment = null;
+//                switch (menuItem.getItemId()) {
+//                    case R.id.action_home:
+//                        fragment = HomeFragment.getInstance();
+//                        break;
+//                    case R.id.action_songs:
+//                        fragment = SongsFragment.getInstance();
+//                        break;
+//                    case R.id.action_search:
+//                        fragment = SearchFragment.getInstance();
+//                        break;
+//                    case R.id.action_content:
+//                        fragment = ContentFragment.getInstance();
+//                        break;
+//
+//                }
+//                replaceFragment(fragment);
+//                return true;
+//            }
+//        });
+//    }
+//
+//    private void setInitialFragment() {
+//        mTransaction.add(R.id.fragment_container, HomeFragment.getInstance());
+//        mTransaction.commit();
+//    }
+//
+//    private void requestPermissions() {
+//        if (ContextCompat.checkSelfPermission(getApplicationContext(),
+//                Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+//
+//            ActivityCompat.requestPermissions(this,
+//                    new String[]{Manifest.permission.RECORD_AUDIO,
+//                            Manifest.permission.MODIFY_AUDIO_SETTINGS}, Constants.PERMISSIONS.MICROPHONE);
+//
+//        } else {
+//            Session.getInstance(this).setAudioEnabled(true);
+//        }
+//    }
+//
+//    private void replaceFragment(Fragment fragment) {
+//        String fragmentTag = getFragmentTag(fragment);
+//        Fragment currentFragment = mFragmentManager.findFragmentByTag(fragmentTag);
+//        if (currentFragment != null) {
+//            if (!currentFragment.isVisible()) {
+//
+//                if (fragment.getArguments() != null) {
+//                    currentFragment.setArguments(fragment.getArguments());
+//                }
+//                mFragmentManager.beginTransaction()
+//                        .replace(R.id.fragment_container, currentFragment, fragmentTag)
+//                        .addToBackStack(null)
+//                        .commit();
+//
+//            }
+//        } else {
+//            mFragmentManager.beginTransaction()
+//                    .replace(R.id.fragment_container, fragment, fragmentTag)
+//                    .addToBackStack(null)
+//                    .commit();
+//        }
+//    }
+//
+//    private String getFragmentTag(Fragment fragment) {
+//        if (fragment instanceof HomeFragment) {
+//            return HomeFragment.TAG;
+//        } else {
+//            if (fragment instanceof SongsFragment) {
+//                return SongsFragment.TAG;
+//            } else {
+//                if (fragment instanceof SearchFragment) {
+//                    return SearchFragment.TAG;
+//                } else {
+//                    return ContentFragment.TAG;
+//                }
+//            }
+//        }
+//    }
+//
+//    @Override
+//    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+//        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+//        System.out.println(requestCode);
+//        if (requestCode == Constants.PERMISSIONS.MICROPHONE) {
+//
+//            if (grantResults.length > 0
+//                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//                Session.getInstance(this).setAudioEnabled(true);
+//            } else {
+//            }
+//            return;
+//        }
+//    }
+//
+    @Override
+    public void onChangeFragment(Fragment fragment) {
+//        replaceFragment(fragment);
+    }
+
+
 }
--- app/src/main/java/vidal/sergi/sallefyv1/controller/fragments/SearchFragment.java
+++ app/src/main/java/vidal/sergi/sallefyv1/controller/fragments/SearchFragment.java
@@ -2,6 +2,8 @@ package vidal.sergi.sallefyv1.controller.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -10,56 +12,33 @@ import android.view.ViewGroup;
 
 import vidal.sergi.sallefyv1.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SearchFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class SearchFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    public static final String TAG = SearchFragment.class.getName();
 
-    public SearchFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SearchFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SearchFragment newInstance(String param1, String param2) {
-        SearchFragment fragment = new SearchFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    public static SearchFragment getInstance() {
+        return new SearchFragment();
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_search, container, false);
+        return v;
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false);
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
     }
 }
