2ed392a2f8a53b329ad8b8cec81feed78e10be83
Jordi Sabanés <43608629+JordiiSM@users.noreply.github.com>
2020-05-05 19:16:20 +0200

    Create ProfileFragment

--- app/src/main/java/vidal/sergi/sallefyv1/controller/fragments/ProfileFragment.java
+++ app/src/main/java/vidal/sergi/sallefyv1/controller/fragments/ProfileFragment.java
@@ -9,11 +9,16 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import vidal.sergi.sallefyv1.R;
+import vidal.sergi.sallefyv1.utils.Session;
 
 public class ProfileFragment extends Fragment {
-
+    private TextView tvName;
+    private TextView tvMail;
+    private TextView tvFollowers;
+    private TextView tvFollowings;
     public static final String TAG = ProfileFragment.class.getName();
 
     public static ProfileFragment getInstance() {
@@ -29,7 +34,21 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_profile, container, false);
+        initViews(v);
         return v;
+    }
+    private void initViews(View v) {
+        tvName = v.findViewById(R.id.name);
+        tvName.setText(Session.getInstance(getContext()).getUser().getLogin());
+
+        tvMail = v.findViewById(R.id.mail);
+        tvMail.setText(Session.getInstance(getContext()).getUser().getEmail());
+
+        tvFollowers = v.findViewById(R.id.followers);
+        tvFollowers.setText("Followers: "+Session.getInstance(getContext()).getUser().getFollowers());
+
+        tvFollowings = v.findViewById(R.id.followings);
+        tvFollowings.setText("Followings: "+Session.getInstance(getContext()).getUser().getFollowing());
     }
 
     @Override
