1311ebaa9631a93d25083e9e810b868af7d880f6
Sergi Vidal Mestre <s.vidal@students.salle.url.edu>
2020-05-07 18:46:39 +0200

    Added functionality to get device location

---
+++ app/src/main/java/vidal/sergi/sallefyv1/model/Location.java
@@ -0,0 +1,44 @@
+package vidal.sergi.sallefyv1.model;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Location {
+    @SerializedName("latitude")
+    private Double latitude;
+
+    @SerializedName("longitude")
+    private Double longitude;
+
+
+    public Location() {
+    }
+
+    public Location(Double latitude, Double longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+
+    @Override
+    public String toString() {
+        return "Location{" +
+                "latitude=" + latitude +
+                ", longitude=" + longitude +
+                '}';
+    }
+}
