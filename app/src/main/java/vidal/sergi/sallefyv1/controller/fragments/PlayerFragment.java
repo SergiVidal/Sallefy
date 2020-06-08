5e45ffd36bdf3aa91af452afc38ee9bffd8afdc8
Joancc97 <joan-cat@hotmail.com>
2020-06-08 22:12:05 +0200

    Final v1.0

--- app/src/main/java/vidal/sergi/sallefyv1/controller/fragments/PlayerFragment.java
+++ app/src/main/java/vidal/sergi/sallefyv1/controller/fragments/PlayerFragment.java
@@ -22,6 +22,8 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
@@ -30,6 +32,8 @@ import com.gauravk.audiovisualizer.visualizer.CircleLineVisualizer;
 import com.google.android.gms.cast.framework.CastButtonFactory;
 import com.google.android.gms.cast.framework.CastContext;
 import com.google.android.gms.cast.framework.CastSession;
+import com.google.android.gms.cast.framework.CastStateListener;
+import com.google.android.gms.cast.framework.IntroductoryOverlay;
 import com.google.android.gms.cast.framework.SessionManagerListener;
 
 import java.io.IOException;
@@ -75,7 +79,10 @@ public class PlayerFragment extends Fragment implements SessionManagerListener<C
     private MenuItem mediaRouteMenuItem;
 
     private Box<Database> userBox;
+    private CastStateListener mCastStateListener;
+    private Toolbar mToolbar;
 
+    private IntroductoryOverlay mIntroductoryOverlay;
     public static PlayerFragment getInstance() {
         return new PlayerFragment();
     }
@@ -97,14 +104,39 @@ public class PlayerFragment extends Fragment implements SessionManagerListener<C
         mCastContext = CastContext.getSharedInstance(getContext()); // inicio del proceso de casteo
         setHasOptionsMenu(true);
     }
-    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater){
-        super.onCreateOptionsMenu(menu, inflater);
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        System.out.println("Entra el OncreateOption");
         inflater.inflate(R.menu.browse, menu);
-        mediaRouteMenuItem = CastButtonFactory.setUpMediaRouteButton(getActivity(), menu , R.id.share_btn);
-
-
+        mediaRouteMenuItem = CastButtonFactory.setUpMediaRouteButton(getActivity(), menu,
+                R.id.media_route_menu_item);
+        super.onCreateOptionsMenu(menu,inflater);
     }
-
+    private void showIntroductoryOverlay() {
+        if (mIntroductoryOverlay != null) {
+            mIntroductoryOverlay.remove();
+        }
+        if ((mediaRouteMenuItem != null) && mediaRouteMenuItem.isVisible()) {
+            new Handler().post(new Runnable() {
+                @Override
+                public void run() {
+                    mIntroductoryOverlay = new IntroductoryOverlay.Builder(
+                            getActivity(), mediaRouteMenuItem)
+                            .setTitleText("Introducing Cast")
+                            .setSingleTime()
+                            .setOnOverlayDismissedListener(
+                                    new IntroductoryOverlay.OnOverlayDismissedListener() {
+                                        @Override
+                                        public void onOverlayDismissed() {
+                                            mIntroductoryOverlay = null;
+                                        }
+                                    })
+                            .build();
+                    mIntroductoryOverlay.show();
+                }
+            });
+        }
+    }
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -121,7 +153,20 @@ public class PlayerFragment extends Fragment implements SessionManagerListener<C
 
         Log.d("Static: ", "Enter onCreate " + this.hashCode());
         initViews(v);
+        mCastContext = CastContext.getSharedInstance(this.getContext()); // inicio del proceso de casteo
+        mediaRouteMenuItem = v.findViewById(R.id.media_route_menu_item);
+
+        setupActionBar(v);
+        showIntroductoryOverlay();
+        setHasOptionsMenu(true);
         return v;
+    }
+    private void setupActionBar(View v) {
+        System.out.println("Seting UP");
+        mToolbar = (Toolbar) v.findViewById(R.id.toolbar);
+        mToolbar.setTitle(R.string.app_name);
+        // mToolbar.set
+        //((AppCompatActivity)getActivity()).setSupportActionBar(mToolbar);
     }
 
     public boolean isDownloaded() {
